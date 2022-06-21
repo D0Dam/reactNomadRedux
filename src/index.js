2f03256bbb9a4630e6161afd51762c54d0e1d3cf
minjaeKim <smallkdb@gmail.com>
2022-06-21 21:58:57 +0900

    make counter with vanilla js and redux

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const num = document.getElementById("num");
+const ADD = "ADD";
+const MINUS = "MINUS";
 
-let count = 0;
-
-const updateCounter = () => {
-	num.innerText = count;
+const counterReducer = (count = 0, action) => {
+	switch(action.type) {
+		case "add":
+			return count+1;
+		case "minus":
+			return count-1;
+		default:
+			return count;
+	}
+	}
 };
-
+let countStore = createStore(counterReducer);
+countStore.subscribe(() => {
+	num.innerText = countStore.getState();
+});
 const handleAdd = () => {
-	count = count + 1;
-	updateCounter();
+	countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-	count = count - 1;
-	updateCounter();
+	countStore.dispatch({ type: MINUS });
 };
+
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
